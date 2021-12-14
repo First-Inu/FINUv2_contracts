43018e2d59e4210b749c8d8f2c0e54e65c4234df
Perfect-SoftwareEngineer <67376797+Perfect-SoftwareEngineer@users.noreply.github.com>
2021-12-15 04:27:59 +0800

    feat(fix): fix transferfrom issue

--- contracts/FINU.sol
+++ contracts/FINU.sol
@@ -117,13 +117,7 @@ contract FINU is Context, IERC20, Ownable {
 
     function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
         _transfer(sender, recipient, amount);
-
-        uint256 currentAllowance = _allowances[sender][_msgSender()];
-        require(currentAllowance >= amount, "ERC20: transfer amount exceeds allowance");
-        unchecked {
-            _approve(sender, _msgSender(), currentAllowance - amount);
-        }
-
+        _approve(sender, _msgSender(), _allowances[sender][_msgSender()].sub(amount, "ERC20: transfer amount exceeds allowance"));
         return true;
     }
 
